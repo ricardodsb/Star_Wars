439a5f2d53b231293940b55f95c5224a60e5f213
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-10-28 19:23:23 +0000

    Star Wars logo + favs dropdown

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,32 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img
+					src="https://icon-library.com/images/star-wars-icon-png/star-wars-icon-png-8.jpg"
+					className="col-1"
+					alt="..."
+				/>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favorites</button>
-				</Link>
+			<div className="dropdown">
+				<button
+					className="btn btn-primary dropdown-toggle"
+					type="button"
+					id="dropdownMenuButton1"
+					data-bs-toggle="dropdown"
+					aria-expanded="false">
+					Favorites
+				</button>
+				<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+					<li>
+						<a className="dropdown-item" href="#" />
+					</li>
+					<li>
+						<a className="dropdown-item" href="#" />
+					</li>
+					<li>
+						<a className="dropdown-item" href="#" />
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
