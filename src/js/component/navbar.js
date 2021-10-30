c0b4c14bd7f87388229968de8b556b4d774811dc
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-10-30 09:21:56 +0000

    navbar finished

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light ">
-			<Link to="/">
-				<img
-					src="https://icon-library.com/images/star-wars-icon-png/star-wars-icon-png-8.jpg"
-					className="col-1"
-					alt="..."
-					style={{ display: "inline-block" }}
-				/>
-			</Link>
-			<div className="dropdown" style={{ display: "inline-block" }}>
+		<nav className="navbar row d-flex justify-content-center col-12 bg-light">
+			<div className="col-4">
+				<Link to="/">
+					<img
+						src="https://icon-library.com/images/star-wars-icon-png/star-wars-icon-png-8.jpg"
+						className="d-grid gap-2 d-md-flex justify-content-md-start"
+						alt="..."
+						style={{ width: "55px", marginLeft: "30px" }}
+					/>
+				</Link>
+			</div>
+			<div className="dropdown col-4 d-grid gap-2 d-md-flex justify-content-md-end">
 				<button
-					className="btn btn-primary dropdown-toggle"
+					className="btn btn-primary dropdown-toggle justify-content-md-end"
 					type="button"
 					id="dropdownMenuButton1"
 					data-bs-toggle="dropdown"
